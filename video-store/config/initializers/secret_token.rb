93c9fed50754fa844fe31114d2f0a03cd5557ad6
Ignacio Parada <ignacio@archer.(none)>
2013-05-22 01:05:33 -0400

    First commit

---
+++ video-store/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VideoStore::Application.config.secret_token = 'd08cf0f43fb5ba71a5f3f0bd40fafa9f58880fd8f82d7c236102ee263bfe8c8677d69f28d0996964043a0e3d02063adad157959c6803751d40bfeac5da81e5c5'
