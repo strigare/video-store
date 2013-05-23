316b28bb649e194ddbff92cc479efb5b5e4a9514
Ignacio Parada <ignacio@archer.(none)>
2013-05-23 03:53:20 -0400

    Working app

--- video-store/app/assets/javascripts/moviesController.js
+++ video-store/app/assets/javascripts/moviesController.js
@@ -1,51 +1,102 @@
 $(document).ready(function () {
 
+  var elementName = 'tests4';
     initialize();
 
-    $('#addMovie').click(createMovie);
     $('#deleteAll').click(clearMovies);
+
+    $( '#addMovieForm' ).on( 'submit', function( event ) {
+      
+      var name = $('#inputName').val();
+      var description = $('#inputDescription').val();
+      var category = $('#inputCategory').val();
+      var yt_id = $('#inputYoutubeId').val();
+      var id = $('#inputId').val();
+      
+      $('#addVideoModal').modal('hide')
+      cleanModalForm();
+
+      postMovie(name, description, category, yt_id, id );
+    });
+
+    $('.categorySelector').click(function(){
+
+      if($(this).html() != 'All'){
+        getMovies(showMovies, $(this).html());
+      } else{
+        getMovies(showMovies);
+      } 
+    });
 
     function initialize(){
         getMovies(showMovies);
+    }
+
+    function cleanModalForm(){
+      $('#inputId').val('');
+      $('#inputName').val('');
+      $('#inputDescription').val('');
+      $('#inputYoutubeId').val('');
     }
 
     function clearMovies(){
         getMovies(deleteAll);
     }
 
-    function createMovie(){
-        postMovie('test', 'test', 'test', 'KrVC5dm5fFc');
-    }
-
     function deleteAll(movies){
         for (var i=0; i<movies.length ; i++) {
-            $.ajax({
-              url: 'http://ignacio.apidone.com/movies/' + movies[i].id,
-              type: 'DELETE',
-              dataType: 'json',
-              data: { },
-              success: function(data, textStatus, xhr) {
-                console.log('Deleted movie');
-              }
-            });
+            deleteMovie(movies[i].id);
         }
     }
 
-    function postMovie( name, description, category, yt_id){
-        $.ajax({
-          url: 'http://ignacio.apidone.com/movies',
-          type: 'POST',
-          dataType: 'json',
-          data: { "name": name, "category": category, "description": description, "yt_id": yt_id},
+    function deleteMovie(id){
+      $.ajax({
+        url: 'http://ignacio.apidone.com/' + elementName + '/' + id,
+        type: 'DELETE',
+        dataType: 'json',
+        data: { },
+        success: function(data, textStatus, xhr) {
+          getMovies(showMovies);
+        }
+      });
+    }
+
+    function postMovie( name, description, category, yt_id, id){
+
+      var parameters = { "name": name, "category": category, "description": description, "yt_id": yt_id};
+      var type = 'POST';
+      var url = 'http://ignacio.apidone.com/' + elementName + ''
+
+      if(id){
+        parameters.id = id;
+        type = 'PUT';
+        url += '/' + id;
+      }
+
+      $.ajax({
+        url: url,
+        type: type,
+        dataType: 'json',
+        data: parameters,
+        success: function(data, textStatus, xhr) {
+          //alert("Id:"+data.id);
+          getMovies(showMovies );
+        }
+      });
+    }
+
+    function getMovie(id, callback){
+      $.ajax({
+          url: 'http://ignacio.apidone.com/' + elementName + '/' + id,
           success: function(data, textStatus, xhr) {
-            alert("Id:"+data.id);
+            callback(data);
           }
         });
     }
 
     function getMovies(callback, category){
 
-        var urlString = 'http://ignacio.apidone.com/movies';
+        var urlString = 'http://ignacio.apidone.com/' + elementName + '';
 
         if(category){
             urlString += '?category=' + category
@@ -59,6 +110,18 @@ $(document).ready(function () {
         });
     }
 
+    function editMovie(id){
+      $('#addVideoModal').modal('show')
+      getMovie(id, fillmodalForm);
+    }
+
+    function fillmodalForm(movie){
+      $('#inputId').val(movie.id);
+      $('#inputName').val(movie.name);
+      $('#inputDescription').val(movie.description);
+      $('#inputYoutubeId').val(movie.yt_id);
+    }
+
     function showMovies(movies){
         var moviesHTML = '<ul class="thumbnails">';
 
@@ -70,8 +133,8 @@ $(document).ready(function () {
                     '<h3>' + movies[i].name + '</h3>'+
                     '<p>' + movies[i].description + '</p>'+
                     '<div align="right">' + 
-                        '<a href="http://bootsnipp.com/" class="btn">Edit</a> \n'+
-                        '<a href="http://bootsnipp.com/" class="btn btn-danger">Delete</a>'+
+                        '<a href="#" movie_id="' + movies[i].id + '" class="btn editMovie">Edit</a> \n'+
+                        '<a href="#" movie_id="' + movies[i].id + '" class="btn btn-danger deleteMovie">Delete</a>'+
                     '</div>' +
                   '</div>'+
                 '</div>'+
@@ -80,6 +143,14 @@ $(document).ready(function () {
 
         moviesHTML += '</ul>';
 
-        $('#movies').append(moviesHTML );
+        $('#movies').html(moviesHTML);
+
+        $('.deleteMovie').click(function(){
+          deleteMovie($(this).attr('movie_id'));
+        });
+
+        $('.editMovie').click(function(){
+          editMovie($(this).attr('movie_id'));
+        });
     }
 });
